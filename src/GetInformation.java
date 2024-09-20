d46ce09c277e23a7cab8e92856a92b748f496869
bonnesbaby14 <bonnesbaby14@gmail.com>
2024-09-19 21:18:39 -0600

    finaly fixes

--- src/GetInformation.java
+++ src/GetInformation.java
@@ -1,14 +1,18 @@
 
-import com.google.gson.FieldNamingPolicy;
+import com.google.gson.*;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import models.Data;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 public class GetInformation {
@@ -19,6 +23,11 @@ public class GetInformation {
 
 
         try {
+
+            LocalDateTime date = LocalDateTime.now();
+            String formatedDate = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+
             URI uri = URI.create("https://v6.exchangerate-api.com/v6/ee1779962d048bab2ded04e8/latest/"+in);
 
             HttpClient client = HttpClient.newHttpClient();
@@ -32,7 +41,7 @@ public class GetInformation {
 
 
             Gson gson= new GsonBuilder()
-
+                    .setPrettyPrinting()
                     .create();
             Data data=gson.fromJson(reponse.body(),Data.class);
 
@@ -43,6 +52,36 @@ public class GetInformation {
 
             System.out.println("El valor de "+value + " "+in + " a "+out+" es: "+result);
 
+            JsonObject nuevoDato = new JsonObject();
+            nuevoDato.addProperty("in", value);
+            nuevoDato.addProperty("out", result);
+            nuevoDato.addProperty("type", in+" to "+out);
+            nuevoDato.addProperty("date", formatedDate);
+
+            File archivo = new File("logs.json");
+            JsonArray dataPrev = new JsonArray();
+
+
+            if (archivo.exists()) {
+                try (FileReader reader = new FileReader(archivo)) {
+                    dataPrev = gson.fromJson(reader, JsonArray.class);
+                    if (dataPrev == null) {
+                        dataPrev = new JsonArray();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error al leer el archivo: " + e.getMessage());
+                }
+            }
+
+
+            dataPrev.add(nuevoDato);
+
+
+            try (FileWriter writer = new FileWriter("logs.json")) {
+                gson.toJson(dataPrev, writer);
+            } catch (IOException e) {
+                System.out.println("Error al escribir en el archivo: " + e.getMessage());
+            }
 
 
 
